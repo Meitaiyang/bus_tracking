3af3f5076a57d928f20537ed2a8efd65d730c7cd
Meitaiyang <shrek473@gmail.com>
2023-08-15 01:14:19 +0800

    add taipei bus api json file.

--- app/models/user.py
+++ app/models/user.py
@@ -3,4 +3,6 @@ from app.extensions import db
 class Users(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    bus_id = db.Column(db.Integer, db.ForeignKey('bus.bus_id'), nullable=False)
+    station_id = db.Column(db.Integer, db.ForeignKey('bus_station.station_id'), nullable=False)
     mappings = db.relationship('BusStationMapping', backref='bus', lazy=True)
--- app/subscribe/routes.py
+++ app/subscribe/routes.py
@@ -2,9 +2,15 @@ from flask import jsonify, request
 from app.subscribe import bp
 from app.extensions import db
 from app.subscribe.task import check_users
+from app.models.users import Users
 
 
-@bp.route('/add')
-def start_add():
+@bp.route('/<string: email>/<string: bus_number>/<string: station_name>')
+def start_add(email, bus_number, station_name):
+
+    # check if the user is already in the database
+    user = Users.query.filter_by(email=email, bus_number).first()
+    if user:
+        return jsonify({"result_id": "user already exists"})
     result = check_users.delay()
     return jsonify({"result_id": result.id})
